13f2c79a3662a78ef17712beef55076cb60b0bfb
unknown <laimecom123@gmail.com>
2025-07-26 21:45:43 -0500

    metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,17 +15,34 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
-      </head>
-      <body>{children}</body>
-    </html>
-  )
+  <html lang="en">
+    <head>
+      <title>rayken</title>
+      <meta name="title" content="rayken" />
+      <meta name="description" content="Created by rayken" />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://are-you-sure.onrender.com/" />
+      <meta property="og:title" content="rayken" />
+      <meta property="og:description" content="Created by rayken" />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://are-you-sure.onrender.com/" />
+      <meta property="twitter:title" content="rayken" />
+      <meta property="twitter:description" content="Created by rayken" />
+      <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+      <style>{`
+        html {
+          font-family: ${GeistSans.style.fontFamily};
+          --font-sans: ${GeistSans.variable};
+          --font-mono: ${GeistMono.variable};
+        }
+      `}</style>
+    </head>
+    <body>{children}</body>
+  </html>
+)
 }
